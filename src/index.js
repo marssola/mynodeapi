ae66713b3ec087b55905fa048791a9acc0a090e6
Mauro Marssola <marssolinha@hotmail.com>
2019-10-29 20:37:47 -0300

    Revert "Vending Machine CLI"
    This reverts commit 3f9ad52b4d9ee2e9d50725f3af518e214caf12c4.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
-const program = require('commander')
-const colors = require('colors')
-const VendingMachine = new (require('./app/VendingMachine'))
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const env = require('./config/env')
 
-program.version('0.1')
-program.option('-a, --all-products', 'Get all products')
-program.option('-i, --product <item>', 'Get produt')
-program.option('-b, --buy', 'Buy product')
-program.option('-p, --pay <value>', 'Payment value')
+const app = express()
 
-program.parse(process.argv)
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-if (program.buy) {
-    let { product, pay } = program
-    if (!product) {
-        console.log(colors.red(`Erro: Produto não informado`))
-        return
-    }
-    let { name, price } = VendingMachine.getProduct(product)
-    if (!name || !price) {
-        console.log(colors.red(`Error: Produto inválido`))
-        return
-    }
-        
-    if (!pay) {
-        console.log(colors.red(`Erro: Você ainda não inseriu nenhum dinheiro para o pagamento`))
-        return
-    }
-    pay = parseFloat(pay)
-    let buy = VendingMachine.buy(price, pay)
-    console.log(`${name} - ${price.toFixed(2)}, pay: ${pay.toFixed(2)}`)
+require('./database')
+require('./app')(app)
 
-    if (typeof buy === 'object')
-        console.log(buy)
-    else if (typeof buy === 'string')
-        console.log(colors.red(buy))
-    return
-}
+const server = http.createServer(app)
+server.listen(env.port, () => {
+    console.log('\x1b[32m%s\x1b[0m', '[ Ok ]', `Env: ${env.env}, port: ${env.port}`)
+})
+// app.listen(3000)
 
-if (program.allProducts) {
-    console.log(VendingMachine.getProducts())
-    return
-}
-
-if (program.product) {
-    console.log(VendingMachine.getProduct(program.product))
-    return
-}
